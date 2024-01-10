47f132d335536eb180e16d4c0a678259aa197e1f
KateFedkova <kate.fedkova@gmail.com>
2024-01-10 18:19:08 +0200

    Write condition if guess is equal to secret number

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct answer";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   }
 });
