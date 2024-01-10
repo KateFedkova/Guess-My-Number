33aa263b3c88035e2511535d8bd67560ece16a3c
KateFedkova <kate.fedkova@gmail.com>
2024-01-10 18:25:19 +0200

    Write condition if guess > secret number

--- script.js
+++ script.js
@@ -12,5 +12,15 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      score--;
+      document.querySelector(".message").textContent = "💥 You lost the game ";
+      document.querySelector(".score").textContent = score;
+    }
   }
 });
