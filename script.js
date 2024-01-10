bf291492e07c325b45588d6a50d8a90cd9b8ca0c
KateFedkova <kate.fedkova@gmail.com>
2024-01-10 18:15:17 +0200

    Write condition if guess is not  given

--- script.js
+++ script.js
@@ -5,4 +5,7 @@ let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number";
+  }
 });
